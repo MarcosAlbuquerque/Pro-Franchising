ae2ec19593d7074c521613468f91ac398de445ff
Marcos Albuquerque <mpdsa20@gmail.com>
2022-03-01 11:22:00 -0300

    Criado campos de login e senha do usuario

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,31 +1,41 @@
-import { useState } from 'react'
-import auth from '../api/auth'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import auth from '../api/auth';
 
 export default function Login() {
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
   return (
-    <form method='POST'>
+    <section>
       <input
-        type="text"
-        name="username"
+        type='text'
+        name='username'
         autoFocus
         required
-        onChange={ (e) => setUsername(e.target.value)} />
+        onChange={(e) => setUsername(e.target.value)}
+      />
       <input
-        type="password"
-        name="password"
+        type='password'
+        name='password'
         required
-        onChange={ (e) => setPassword(e.target.value)} />
-      <input
-        type="submit"
-        name="entrar"
-        value="Entrar"
-        onClick={ (e) => {
-          auth(username, password);
-          e.preventDefault()
-        } } />
-    </form>
-  )
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button
+        name='entrar'
+        value='Entrar'
+        onClick={() => {
+          auth(username, password).then((res) => {
+            if (document.cookie.slice(6) === res) {
+              navigate({ to: '/receitas' });
+            }
+          });
+        }}
+      >
+        Entrar
+      </button>
+      <span id='invalidMessage'></span>
+    </section>
+  );
 }
