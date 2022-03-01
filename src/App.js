61a9eb617162c193ad80c25d3fb26fe2cdf3b4f6
Marcos Albuquerque <marcosdevmaster@gmail.com>
2022-03-01 13:10:50 -0300

    Revert "Criado rotas para navegação"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 import Login from './pages/Login'
-import Recipes from './pages/Recipes'
-
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={ <Login /> } />
-        <Route exact path="/receitas" element={ <Recipes /> } />
-      </Routes>
-    </BrowserRouter>
-  )
+  return <Login />
 }
 
 export default App;
