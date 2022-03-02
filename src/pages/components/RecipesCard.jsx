1e0e9ff771d0c32208bb1c9a3e78974cd8df6f03
Marcos Albuquerque <mpdsa20@gmail.com>
2022-03-02 06:35:30 -0300

    Redirecionando para editar produto, criado botao para excluir item

--- src/pages/components/RecipesCard.jsx
+++ src/pages/components/RecipesCard.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { nanoid } from 'nanoid';
+import { useNavigate } from 'react-router-dom';
+import profranchisingAPI from '../../api/products';
 
 function RecipeCard({ props }) {
+  const navigate = useNavigate();
+
   return props.map((item) => {
     const { name, image, price, id } = item;
 
@@ -12,6 +16,20 @@ function RecipeCard({ props }) {
           <h1 key={nanoid()}>{name}</h1>
           <p>Preço R${price}</p>
           <p>ID:{id}</p>
+          <button
+            onClick={() => {
+              navigate('/editarreceita', { state: item });
+            }}
+          >
+            Editar
+          </button>
+          <button
+            onClick={() => {
+              profranchisingAPI.deleteProduct(id);
+            }}
+          >
+            Excluir
+          </button>
         </div>
       </div>
     );
