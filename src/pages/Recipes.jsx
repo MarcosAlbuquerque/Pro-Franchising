521f92318cf6346acc61bc6e2975f78aa38d9901
Marcos Albuquerque <mpdsa20@gmail.com>
2022-03-02 12:18:13 -0300

    Definicao de itens por selecao, paginaçao criada

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -8,6 +8,9 @@ import FooterMenu from './components/FooterMenu';
 export default function Recipes() {
   const [itemsAPI, setItemsAPI] = useState();
   const [loaded, setLoaded] = useState(false);
+  const [totalItems, setTotalItems] = useState(5);
+  const [totalPages, setTotalPages] = useState(0);
+  const [offSet, setOffSet] = useState(0);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -15,11 +18,13 @@ export default function Recipes() {
       document.cookie.split(';')[1].slice(7);
 
       if (!itemsAPI) {
-        proFranchisingAPI.getProducts().then((res) => {
+        proFranchisingAPI.getProducts(offSet, totalItems).then((data) => {
           const listItemsAPI = [];
-          res.content.map((item) => listItemsAPI.push(item));
+
+          data.content.map((item) => listItemsAPI.push(item));
           setItemsAPI(listItemsAPI);
           setLoaded(true);
+          setTotalPages(data.totalPages);
         });
       }
     } catch (error) {
@@ -35,7 +40,66 @@ export default function Recipes() {
       ) : (
         <>
           <h1>Produtos</h1>
+          <div>
+            <strong>Defina a quantidade de itens por página </strong>
+            <select onChange={(e) => setTotalItems(Number(e.target.value))}>
+              <option value='5'>5 itens</option>
+              <option value='15'>15 itens</option>
+              <option value='30'>30 itens</option>
+            </select>
+            <button
+              onClick={() => {
+                setLoaded(false);
+                proFranchisingAPI.getProducts(offSet, totalItems).then((data) => {
+                  const listItemsAPI = [];
+
+                  data.content.map((item) => listItemsAPI.push(item));
+                  setItemsAPI(listItemsAPI);
+                  setLoaded(true);
+                  setTotalPages(data.totalPages);
+                  setOffSet(0);
+                });
+              }}
+            >
+              Atualizar
+            </button>
+          </div>
           <RecipeCard props={itemsAPI} />
+          <button
+            onClick={() => {
+              offSet === 0 ? setOffSet(0) : setOffSet((prev) => prev - 1);
+
+              proFranchisingAPI.getProducts(offSet, totalItems).then((data) => {
+                const listItemsAPI = [];
+
+                data.content.map((item) => listItemsAPI.push(item));
+                setItemsAPI(listItemsAPI);
+                setLoaded(true);
+                setTotalPages(data.totalPages);
+              });
+            }}
+          >
+            Voltar Página
+          </button>
+          <button
+            onClick={() => {
+              offSet > totalPages ? setOffSet(totalPages) : setOffSet((prev) => prev + 1);
+
+              proFranchisingAPI.getProducts(offSet, totalItems).then((data) => {
+                const listItemsAPI = [];
+
+                data.content.map((item) => listItemsAPI.push(item));
+                setItemsAPI(listItemsAPI);
+                setLoaded(true);
+                setTotalPages(data.totalPages);
+              });
+            }}
+          >
+            Próxima Página
+          </button>
+          <span>
+            Página: {offSet} de {totalPages}
+          </span>
         </>
       )}
       <FooterMenu />
