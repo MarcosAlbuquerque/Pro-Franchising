12c6c91032a4058d61eec935183ac883ed8bef76
Marcos Albuquerque <mpdsa20@gmail.com>
2022-03-02 06:24:41 -0300

    Criado rota para editar receitas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import CreateItem from './pages/CreateItem'
 
 import Login from './pages/Login'
 import Recipes from './pages/Recipes'
-
+import CreateItem from './pages/CreateItem'
+import EditItem from './pages/EditItem'
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
         <Route exact path="/" element={ <Login /> } />
         <Route exact path="/receitas" element={ <Recipes /> } />
         <Route exact path="/novareceita" element={ <CreateItem /> } />
+        <Route exact path="/editarreceita" element={ <EditItem /> } />
       </Routes>
     </BrowserRouter>
   )
