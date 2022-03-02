0ea4947536d42704c799dabfa989854584354c8d
Marcos Albuquerque <mpdsa20@gmail.com>
2022-03-01 22:41:43 -0300

    Comparando token do browser com token da api

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ export default function Login() {
         value='Entrar'
         onClick={() => {
           auth(username, password).then((res) => {
-            if (document.cookie.slice(6) === res) {
+            if (document.cookie.split(';')[1].slice(7) === res) {
               navigate('/receitas');
             }
           });
